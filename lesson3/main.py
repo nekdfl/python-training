8cab1234b99316d46da224e29e113c536057c11e
Garkushenko Nick <nekdfl@gmail.com>
2022-06-30 12:27:56 +0300

    lesson3

---
+++ lesson3/main.py
@@ -0,0 +1,23 @@
+sym = 'A'
+dec_code_sym = ord('A')
+hex_code = hex(65)
+dec_sym_code = 65
+
+# if hex_code == hex(dec_code_sym):
+#     print("ok")
+# else:
+#     print("err")
+
+# print(ord(u'あ'))
+# print(chr(12354))
+
+dec_number = 1234
+
+print(hex(dec_number))
+print(bin(dec_number))
+
+hex_number = hex(dec_number)
+bin_number = bin(dec_number)
+
+print(int(bin_number, 2))
+print(int(hex_number, 16))
