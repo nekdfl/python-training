4fc841bf07ce99d2430988eba5f206ffee375564
Garkushenko Nick <nekdfl@gmail.com>
2022-07-16 18:51:34 +0300

    lesson6

---
+++ lesson6/class_example.py
@@ -0,0 +1,59 @@
+# import os
+#
+#
+# class Animal(object):
+#
+#     def __init__(self, name):
+#         pass
+#         # public
+#         self.petname = name
+#         # protected
+#         self._name = name
+#         # private
+#         self.__age = "1"
+#
+#     def say_age(self):
+#         print(f"my age {self.__age}")
+#
+#     def say(self):
+#         print("i am pet")
+#
+#
+#
+# class Dog(Animal):
+#
+#     def __init__(self):
+#         pass
+#
+#     def say(self):
+#         print("i am dog")
+#
+#     def catch_cat(self):
+#         pass
+#
+#
+# class Cat(Animal):
+#
+#     def __init__(self):
+#         pass
+#
+#     def say(self):
+#         print("i am cat")
+#
+#     def run_from_dog(self):
+#         pass
+#
+#
+# def main():
+#     pass
+#     myanimal = Animal("pet")
+#     myanimal.name
+#     mycat = Cat()
+#     mydog = Dog()
+#
+#     mycat.say()
+#     mydog.say()
+#
+#
+# if __name__ == '__main__':
+#     main()
